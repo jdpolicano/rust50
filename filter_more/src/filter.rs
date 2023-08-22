ec291c01f65320ae9ed92cad67c10c9ad8851394
= <=>
2023-08-22 09:19:53 -0400

    added allow unused and a note to remove them

--- filter_more/src/filter.rs
+++ filter_more/src/filter.rs
@@ -2,18 +2,23 @@
 use crate::bmp::{RGBTriple};
 // For the purposes of the exercise, we'll define these contracts to edit the bitmap in-place.
 // This means that we'll be mutating the RGBTriple values in the Vec<Vec<RGBTriple>>.
+
+#[allow(unused)] // Remove this line once you start working on the functions
 pub fn blur(rgb_triple: &mut Vec<Vec<RGBTriple>>) {
     todo!("Implement blur")
 }
 
+#[allow(unused)] // Remove this line once you start working on the functions
 pub fn edges(rgb_triple: &mut Vec<Vec<RGBTriple>>) {
     todo!("Implement edges")
 }
 
+#[allow(unused)] // Remove this line once you start working on the functions
 pub fn grey_scale(rgb_triple: &mut Vec<Vec<RGBTriple>>) {
     todo!("Implement grey_scale")
 }
 
+#[allow(unused)] // Remove this line once you start working on the functions
 pub fn reflect(rgb_triple: &mut Vec<Vec<RGBTriple>>) {
     todo!("Implement reflect")
 }
